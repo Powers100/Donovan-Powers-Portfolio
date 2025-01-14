1270ed1196499e2484946aae7c5b372bdcd10382
Powers100 <123561887+Powers100@users.noreply.github.com>
2025-01-14 11:39:53 -0500

    BibleVerseCalculatorGUIVersion.py

--- BibleVerseCalculatorGUIVersion.py
+++ BibleVerseCalculatorGUIVersion.py
@@ -1,12 +1,13 @@
-
 ''' 
-    Create app that allows the user to 1.enter the number of people reading, 2.number of
-    verses in chapter, 3.how many verses each person would like to read (or option
-    to suggest the number of verses to read based on number of people reading and
-    number of verses in the chapter to split evenly), 4.ability to add people's
-    names and then display whose turn it is to read, 5.ability to enter that one person
-    has finished reading and then display who should read next based on user entered 
-    order of reading, 6.display which verses the next person should read.
+The purpose of this simple application is to allow the user to perform the following tasks: 
+1. Enter the number of people reading,
+2. Enter the number of verses in the chapter being read,
+3. Enter how many verses each person would like to read (or have the option to allow the app to suggest the number of verses to read based on number of people reading and number of verses in the chapter to split evenly),
+4. Ability to add people's names and then display whose turn it is to read,
+5. Ability to enter that one person has finished reading and then display who should read next based on the order of reading entered by the user,
+6. Display which verses the next person should read.
+7. Tell the user when they have finished reading.
+8. Provide a simple GUI for the user to input, and view the application's output. (Using PySimpleGUI)
 '''
 import PySimpleGUI as sg
 
